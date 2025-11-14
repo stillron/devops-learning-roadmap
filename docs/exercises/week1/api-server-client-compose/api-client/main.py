2e6b4500d7bf514ce56e11a578fe64fe6bf3b180
Ron Houk <ron.houk@leblibrary.com>
2025-11-14 16:35:31 -0500

    feat: Have a working multi-container version of flask-api-server and api-client with docker compose

--- docs/exercises/week1/api-server-client-compose/api-client/main.py
+++ docs/exercises/week1/api-server-client-compose/api-client/main.py
@@ -1,22 +1,22 @@
 import requests
 
-r = requests.get('http://localhost:5000/containers')
+r = requests.get('http://flask-api-server:5000/containers')
 print(r.json())
 
 
 payload = {"name": "test-container", "image": "ubuntu:22.04", "ports": [9000], "status": "running"}
-r = requests.post('http://localhost:5000/container', json=payload)
+r = requests.post('http://flask-api-server:5000/container', json=payload)
 print(r.json())
 
 payload =  {"name": "test-container", "image": "ubuntu:22.04", "ports": [9000], "status": "running"}
-r = requests.put('http://localhost:5000/container', json=payload)
+r = requests.put('http://flask-api-server:5000/container', json=payload)
 print(r.json())
 
 payload = {"name": "test-container"}
-r = requests.delete('http://localhost:5000/container', json=payload)
+r = requests.delete('http://flask-api-server:5000/container', json=payload)
 print(r.status_code)
 print(r.text)
 
-r = requests.get('http://localhost:5000/containers')
+r = requests.get('http://flask-api-server:5000/containers')
 print(r.json())
 
