c960a86f08746ae5f26e341eb9052b4ae0de1b40
Ron Houk <ron.houk@leblibrary.com>
2025-11-19 16:06:14 -0500

    feat: Added post route forfirst-psycopg-test

--- docs/exercises/week2/01-first-psycopg-test/app.py
+++ docs/exercises/week2/01-first-psycopg-test/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 import psycopg
 from psycopg.rows import dict_row
 
@@ -6,9 +7,30 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello_world():
+    return "Hello World!"
+
+@app.get("/message")
+def show_messages():
     with psycopg.connect("dbname=test_db user=ron.houk", row_factory=dict_row) as conn:
         with conn.cursor() as cur:
 
             records =  cur.execute("SELECT * from test_messages").fetchall()
 
     return records
+
+@app.post("/message")
+def add_message():
+    message = request.json['message']
+
+    with psycopg.connect("dbname=test_db user=ron.houk", row_factory=dict_row) as conn:
+        with conn.cursor() as cur:
+
+            cur.execute("""
+            INSERT INTO test_messages (message)
+            VALUES (%s)
+            RETURNING id, message;
+            """,
+            (message,))
+
+            inserted = cur.fetchone()
+    return {"message": "Success!", "inserted": inserted}
