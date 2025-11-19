16665d3201e21fc7ea45a677248755f8f94f132b
Ron Houk <ron.houk@leblibrary.com>
2025-11-19 09:18:19 -0500

    feat: completed first Flask + PostgreSQL integration

--- docs/exercises/week2/01-first-psycopg-test/app.py
+++ docs/exercises/week2/01-first-psycopg-test/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 import psycopg
+from psycopg.rows import dict_row
 
 app = Flask(__name__)
 
 @app.get("/")
 def hello_world():
-    return "Hello world!"
+    with psycopg.connect("dbname=test_db user=ron.houk", row_factory=dict_row) as conn:
+        with conn.cursor() as cur:
+
+            records =  cur.execute("SELECT * from test_messages").fetchall()
+
+    return records
