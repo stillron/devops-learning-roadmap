c7ca54cd39e3d27b6b8a28098139f54b80d50e46
Ron Houk <ron.houk@leblibrary.com>
2025-11-29 11:51:45 -0500

    feat: started working on the logging lesson

---
+++ exercises/week-03/03-learn-logging/app.py
@@ -0,0 +1,20 @@
+import logging
+from flask import Flask
+from pythonjsonlogger import jsonlogger
+
+# Create a JSON formatter
+logHandler = logging.StreamHandler()
+formatter = jsonlogger.JsonFormatter('%(asctime)s %(levelname)s %(name)s %(message)s')
+logHandler.setFormatter(formatter)
+
+# Setup the logger
+logger = logging.getLogger()
+logger.addHandler(logHandler)
+logger.setLevel(logging.DEBUG)
+
+app = Flask(__name__)
+
+@app.get("/")
+def hello():
+    logger.info("Hello endpoint was called", extra={"endpoint": "/", "method": "GET"})
+    return {"msg": "Hello World!"}, 200
