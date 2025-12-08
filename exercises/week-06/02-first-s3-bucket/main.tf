388a369708664482f2765d8019748f2c35aecc59
Ron Houk <ron.houk@leblibrary.com>
2025-12-08 16:56:25 -0500

    feat: Added file and explicitly stated private bucket

--- exercises/week-06/02-first-s3-bucket/main.tf
+++ exercises/week-06/02-first-s3-bucket/main.tf
@@ -16,4 +16,24 @@ provider "aws" {
 # Create an S3 bucket
 resource "aws_s3_bucket" "my_first_bucket" {
     bucket = "ron-terraform-test-bucket-04040"
+
+    tags = {
+        Name        = "My first Terraform bucket"
+        Environment = "Learning"
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "my_first_bucket" {
+    bucket = aws_s3_bucket.my_first_bucket.id
+
+    block_public_acls   = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
+
+resource "aws_s3_object" "test_file" {
+    bucket = aws_s3_bucket.my_first_bucket.id
+    key = "hello.txt"
+    content = "Hello from Terraform S3!"
 }
